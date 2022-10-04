ae455c853ed591f928fe4c36a375c0b5ddf1abec
efecetinalp <efecetinalp@gmail.com>
2022-10-04 21:41:30 +0300

    Create command

---
+++ Kodlama.io.Devs/WebAPI/Controllers/ProgrammingLanguagesController.cs
@@ -0,0 +1,43 @@
+﻿using Application.Features.ProgrammingLanguages.Commands.CreateProgrammingLanguage;
+using Application.Features.ProgrammingLanguages.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProgrammingLanguagesController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateProgrammingLanguageCommand createProgrammingLanguageCommand)
+        {
+            CreatedProgrammingLanguageDto result = await Mediator.Send(createProgrammingLanguageCommand);
+            return Created("", result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetList()
+        {
+            return Ok();
+        }
+
+        [HttpGet("getbyid")]
+        public async Task<IActionResult> GetById()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete()
+        {
+            return Ok();
+        }
+    }
+}
